0fa5898091f69dada298a3425c7c9dde516cc6c6
Sriram Gopalakrishnan <srigokrish@gmail.com>
2017-01-03 09:08:25 -0700

    Initial Commit - Android Studio Project Created

---
+++ app/src/main/java/com/goperez/neuraudio/MainActivity.java
@@ -0,0 +1,13 @@
+package com.goperez.neuraudio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
