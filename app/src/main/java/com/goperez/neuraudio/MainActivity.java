8598bc8b86055e0101d233500f799a2a71ebc5b2
Sriram Gopalakrishnan <srigokrish@gmail.com>
2017-01-20 08:50:35 -0700

    Changed JFugue library to the GitHub one for Android and added button toggle functionality.

--- app/src/main/java/com/goperez/neuraudio/MainActivity.java
+++ app/src/main/java/com/goperez/neuraudio/MainActivity.java
@@ -2,12 +2,27 @@ package com.goperez.neuraudio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickPlay(View view) {
+        //Play button
+        android.support.design.widget.FloatingActionButton playButton = (android.support.design.widget.FloatingActionButton) findViewById(R.id.myFAB);
+        //Stores state of button
+        if(isPlaying) {
+            isPlaying = false;
+            playButton.setImageResource(R.drawable.ic_play_arrow_white_24dp);
+        } else {
+            isPlaying = true;
+            playButton.setImageResource(R.drawable.ic_pause_white_24dp);
+        }
     }
 }
