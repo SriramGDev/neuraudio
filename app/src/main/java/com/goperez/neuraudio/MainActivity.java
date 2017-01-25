a51319821efc9f4c49a0c4fb7482ac744689b5c7
Sriram Gopalakrishnan <srigokrish@gmail.com>
2017-01-24 20:38:20 -0700

    Wrote code for polyphonic (totally random) music generation of ten notes. No transition probability matrices yet.

--- app/src/main/java/com/goperez/neuraudio/MainActivity.java
+++ app/src/main/java/com/goperez/neuraudio/MainActivity.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Random;
 
 import jm.music.data.Note;
 import jm.music.data.Part;
@@ -43,13 +44,29 @@ public class MainActivity extends AppCompatActivity {
         } else {
             isPlaying = true;
             playButton.setImageResource(R.drawable.ic_pause_white_24dp);
-            Note n = new Note(C4, MINIM);
+            Note[] notes = new Note[10];
+            Note[] notes2 = new Note[10];
+            Random rand = new Random();
+
+            for(int i = 0; i < 10; i++) {
+                notes[i] = new Note(rand.nextInt(37) + 36, new Double(.25) + new Double(2.75) * rand.nextDouble());
+            }
+
+            for(int i = 0; i < 10; i++) {
+                notes2[i] = new Note(rand.nextInt(37) + 36, new Double(.25) + new Double(2.75) * rand.nextDouble());
+            }
+
             Phrase phrase = new Phrase();
-            phrase.addNote(n);
+            phrase.addNoteList(notes);
+            Phrase phrase2 = new Phrase();
+            phrase2.addNoteList(notes2);
+            Part part2 = new Part();
+            part2.addPhrase(phrase2);
             Part part = new Part();
             part.addPhrase(phrase);
             Score score = new Score("Test");
             score.addPart(part);
+            score.addPart(part2);
             Write.midi(score, getFilesDir() + "/test.midi");
             System.out.println(new File(getFilesDir() + "/test.midi").exists());
             playMusic();
